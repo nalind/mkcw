f1e810625dffc0a290ef349dc17c91055eca57ed
Nalin Dahyabhai <nalin@redhat.com>
2023-07-26 17:16:06 -0400

    Update to a newer lukstool
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- vendor/github.com/nalind/lukstool/luks.go
+++ vendor/github.com/nalind/lukstool/luks.go
@@ -43,7 +43,10 @@ func ReadHeaders(f *os.File, options ReadHeaderOptions) (*V1Header, *V2Header, *
 		if size < 4096 {
 			return nil, nil, nil, nil, fmt.Errorf("unsupported header size while looking for JSON data")
 		}
-		if n, err = f.ReadAt(v2b[:], int64(size)); err != nil {
+		if n, err = f.ReadAt(v2b[:], int64(size)); err != nil || n != len(v2b) {
+			if err == nil && n != len(v2b) {
+				err = fmt.Errorf("short read: read only %d bytes, should have read %d", n, len(v2b))
+			}
 			return nil, nil, nil, nil, err
 		}
 		if v2b.Magic() != V2Magic2 {
